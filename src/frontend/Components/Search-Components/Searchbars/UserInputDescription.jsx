5f43a07d6fadb0f1015110abcd4935320b8e1227
Richard Colachagua <richardcolachagua@gmail.com>
2025-06-04 11:52:27 -0400

    Various functionality and UI changes

--- src/frontend/Components/Search-Components/Searchbars/UserInputDescription.jsx
+++ src/frontend/Components/Search-Components/Searchbars/UserInputDescription.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../../../backend/firebase";
